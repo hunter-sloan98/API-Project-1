1b7f3158d63a74e1ae9c37f4e55d2e5e124b9aee
hunter-sloan98 <sloan.hunter98@gmail.com>
2021-08-03 16:44:12 -0400

    styling and basic function

--- api-script.js
+++ api-script.js
@@ -1,17 +1,34 @@
-const baseUrl = 'https://pokeapi.co/api/v2/pokemon/'
+const baseUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1118'
 
+const searchForm = document.getElementsByTagName('form')[0];
+const pokemonList = document.querySelector('p')
 
+searchForm.addEventListener('submit', getPokemon)
 
-
-
-fetch('https://pokeapi.co/api/v2/pokemon/')
-.then(res => res.json())
-.then(data => {
-    console.log(data)
-})
-
-function displayResults {
-
+function getPokemon(e) {
+    e.preventDefault();
+        console.log('Clicked!')
+            fetch(baseUrl)
+                .then(result => {
+                    return result.json();
+                })
+                .then(json => {
+                    displayPokemon(json)
+                })
 }
+function displayPokemon(data){
+    console.log('Inside displayPokemon:', data);
+    function data.forEach(results => {
+        console.log(results.name);
+        let listItem = document.createElement('p');
+        listItem.innerText = pokemon.name;
+        pokemonList.appendChild(listItem);
+        
+    })
+}
+
+
+
+
 
 
